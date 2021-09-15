aa7ad042b4634f28d0572b6a15f31e36213584ef
Zanderlx <ivan.zastawny@gmail.com>
2021-09-15 01:02:30 -0300

    4 Zoom

--- server-files/client_packages/events/events.js
+++ server-files/client_packages/events/events.js
@@ -1,19 +1,19 @@
 let camaraOn = 0 
+let cam
 mp.events.add("toggleCamara",(toggle) => {
     camaraOn = toggle
     if(toggle) {
         var camPos = new mp.Vector3(973.7493896484375,86.74250030517578,85.07453155517578)
         var plaPos = mp.players.local.position
-        let scaleform = mp.game.graphics.requestScaleformMovie("security_cam");
     
-        let cam = mp.cameras.new('DEFAULT_SCRIPTED_FLY_CAMERA', camPos, mp.players.local.getRotation(5), 45);
-        cam.pointAtCoord(plaPos.x, plaPos.y, plaPos.z);
+        cam = mp.cameras.new('DEFAULT_SCRIPTED_FLY_CAMERA', camPos, mp.players.local.getRotation(5), 45)
+        cam.pointAtCoord(plaPos.x, plaPos.y, plaPos.z)
         mp.game.ui.displayRadar(false)
         mp.game.ui.displayHud(false)
         
     }else {
-        mp.game.cam.renderScriptCams(false, false, 0, false, false);
-        mp.game.graphics.setTimecycleModifier("default");
+        mp.game.cam.renderScriptCams(false, false, 0, false, false)
+        mp.game.graphics.setTimecycleModifier("default")
         mp.game.ui.displayHud(true)
         mp.game.ui.displayRadar(true);
     }
@@ -21,6 +21,34 @@ mp.events.add("toggleCamara",(toggle) => {
 
 mp.events.add('render', () => { 
     if (camaraOn) {
-        mp.game.cam.renderScriptCams(true, false, 0, true, false);
+        mp.game.cam.renderScriptCams(true, false, 0, true, false)
     }
 })
+
+//Zoom camara
+const zoomSpeed = 1
+const minZoom = 10.0
+const maxZoom = 50.0
+
+mp.events.add('render', () => {
+    if (camaraOn) {
+        mp.game.controls.disableAllControlActions(2)
+
+        let zoomIn = (mp.game.controls.getDisabledControlNormal(2, 40) * zoomSpeed)
+        let zoomOut = (mp.game.controls.getDisabledControlNormal(2, 41) * zoomSpeed)
+
+        if (zoomIn > 0) {
+            let currentFov = cam.getFov()
+            currentFov -= zoomIn
+            if (currentFov < minZoom)
+                currentFov = minZoom
+            cam.setFov(currentFov)
+        } else if (zoomOut > 0) {
+            let currentFov = cam.getFov()
+            currentFov += zoomOut
+            if (currentFov > maxZoom)
+                currentFov = maxZoom
+            cam.setFov(currentFov)
+        }
+    }
+});
